f6eb1ba95869fd5e5e98cb871a6b8c45313e1050
vhssa <vhssa@cin.ufpe.br>
2020-12-28 00:32:22 -0300

    chap_01 first programs

---
+++ chap_01/square_of_a_number/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+double square(double x){
+    return x*x;
+}
+
+void print_square(double x){
+    std::cout << "the square of " << x << " is " << square(x) << "\n";
+}
+
+int main(){
+    print_square(1.234);
+}
